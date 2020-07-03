9cec7599f1861bfd89fb28439c5534044fc4017a
Magnufn <magnusfn@hotmail.com>
2020-07-03 22:50:33 +0200

    working map, but mutates in state

--- src/components/game.js
+++ src/components/game.js
@@ -18,8 +18,14 @@ class Game extends Component {
           var ran = Math.floor(Math.random() * 9 + 1) //returns int between 0-9 + 1
           console.log(ran)
           var a = pokemonMap.get(ran);
-          this.setState({points: this.state.points - 0, this.myPokemons.set(ran, a)})
-          //this.setState({myPokemons: [0, 1]})//this.myPokemons.set(ran, a)}) //fungerer ikke
+          console.log("a", a);
+          this.setState({points: this.state.points - 0})
+          if(!this.state.myPokemons.has(ran)){
+            this.setState({myPokemons: this.state.myPokemons.set(ran, 1)}) //fungerer ikke
+          }
+          else {
+            this.setState({myPokemons: this.state.myPokemons.set(ran, this.state.myPokemons.get(ran) + 1)})
+          }
           console.log(pokemonMap.get(ran))
           console.log("mp", this.state.myPokemons)
         } 
